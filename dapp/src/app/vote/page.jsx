d7648cce613a0aac0b4c802942e50c9b83d041a9
adrianovolter <adriano.j.volter@hotmail.com>
2024-02-03 16:38:05 -0300

    funçoes de votaçõ

--- dapp/src/app/vote/page.jsx
+++ dapp/src/app/vote/page.jsx
@@ -2,29 +2,30 @@
 
 import { useState, useEffect } from "react";
 import Head from "next/head";
-import { doLogin } from "@/services/Web3Service";
 import { getCurrentVoting, addVote } from "@/services/Web3Service";
 import { useRouter } from "next/navigation";
 
 export default function Vote() {
   const { push } = useRouter();
+
   const DEFAULT_OPTION = {
-    name: "Loading....",
+    name: "Loading...",
     image:
-      "https://cdn.pixabay.com/photo/2017/10/24/07/12/hacker-2883630_1280.jpg",
+      "https://cdn.pixabay.com/photo/2020/05/09/21/43/hacker-5151533_1280.jpg",
   };
+
   const [message, setMessage] = useState("");
   const [voting, setVoting] = useState({ maxDate: Date.now() });
   const [option1, setOption1] = useState(DEFAULT_OPTION);
   const [option2, setOption2] = useState(DEFAULT_OPTION);
-  const [showVote, setShowVote] = useState(0);
+  const [showVotes, setShowVotes] = useState(0);
 
   useEffect(() => {
     if (!localStorage.getItem("wallet")) return push("/");
+
     getCurrentVoting()
       .then((voting) => {
         console.log(voting);
-        //@ts-ignore
         setVoting(voting);
         setOption1(getOption(voting.option1));
         setOption2(getOption(voting.option2));
@@ -84,19 +85,15 @@ export default function Vote() {
     <>
       <Head>
         <title>Webbb3 | Votação</title>
-        <meta name="description" content="Webbb3" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className="container col-xxl-8 py-5">
-        <div className="row align-items-center g-5 py-5">
+      <div className="container col-xxl-8 px-4 py-5">
+        <div className="row align-items-center">
           <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">
             Webbb3
           </h1>
-          <p className="lead text-danger">
-            User: {localStorage.getItem("wallet")}
-          </p>
-          <p className="lead">Votação on-chain do BBB, </p>
+          <p className="lead">Votação on-chain do BBB.</p>
           {voting.maxDate > Date.now() / 1000 ? (
             <p className="lead mb-3">
               Você tem até {new Date(Number(voting.maxDate) * 1000).toString()}{" "}
@@ -105,7 +102,7 @@ export default function Vote() {
             </p>
           ) : (
             <p className="lead mb-3">
-              A votação para este paredão já encerrou.
+              Votação encerrada. Confira abaixo os resultados.
             </p>
           )}
         </div>
@@ -118,26 +115,27 @@ export default function Vote() {
             <img
               src={option2.image}
               className="d-block mx-auto img-fluid rounded"
-              style={{ width: 250 }}
+              width={250}
+              height={250}
             />
-            {showVote > 0 || voting.maxDate < Date.now() / 1000 ? (
+            {showVotes > 0 || voting.maxDate < Date.now() / 1000 ? (
               <button
-                className="btn btn-secondary p-3 d-block mx-auto my-2"
+                className="btn btn-secondary p-3 my-2 d-block mx-auto"
                 style={{ width: 250 }}
                 disabled={true}
               >
-                {showVote === 2
+                {showVotes === 2
                   ? Number(voting.votes2) + 1
                   : Number(voting.votes2)}{" "}
                 votos
               </button>
             ) : (
               <button
-                className="btn btn-primary p-3 d-block mx-auto my-2"
+                className="btn btn-primary p-3 my-2 d-block mx-auto"
                 style={{ width: 250 }}
                 onClick={btnVote2Click}
               >
-                Quero que sai esse!
+                Quero que saia esse
               </button>
             )}
           </div>
@@ -148,29 +146,33 @@ export default function Vote() {
             <img
               src={option1.image}
               className="d-block mx-auto img-fluid rounded"
-              style={{ width: 250 }}
+              width={250}
+              height={250}
             />
-            {showVote > 0 || voting.maxDate < Date.now() / 1000 ? (
+            {showVotes > 0 || voting.maxDate < Date.now() / 1000 ? (
               <button
-                className="btn btn-secondary p-3 d-block mx-auto my-2"
+                className="btn btn-secondary p-3 my-2 d-block mx-auto"
                 style={{ width: 250 }}
                 disabled={true}
               >
-                {showVote === 1
+                {showVotes === 1
                   ? Number(voting.votes1) + 1
                   : Number(voting.votes1)}{" "}
                 votos
               </button>
             ) : (
               <button
-                className="btn btn-primary p-3 d-block mx-auto my-2"
+                className="btn btn-primary p-3 my-2 d-block mx-auto"
                 style={{ width: 250 }}
                 onClick={btnVote1Click}
               >
-                Quero que sai esse!
+                Quero que saia esse
               </button>
             )}
           </div>
+        </div>
+        <div className="row align-items-center">
+          <p className="message">{message}</p>
         </div>
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <p className="col-md-4 mb-0 text-body-secondary">
--- dapp/src/services/Web3Service.js
+++ dapp/src/services/Web3Service.js
@@ -1,34 +1,34 @@
-import Web3 from 'web3';
-import ABI from './ABI.json';
+import Web3 from "web3";
+import ABI from "./ABI.json";
 
-const CONTRACT_ADDRAESS = '0x6D66d7674675fDe6fd4d31129c8745Dd808b4cA1';
+const CONTRACT_ADDRESS = "0x6D66d7674675fDe6fd4d31129c8745Dd808b4cA1";
 
 export async function doLogin() {
-    if (!window.ethereum) throw new Error('Please install MetaMask');
+  if (!window.ethereum) throw new Error("No MetaMask found.");
 
-    const web3 = new Web3(window.ethereum);
-    const accounts = await web3.eth.requestAccounts();
-    if(!accounts || !accounts.length) throw new Error('Wallet not found/allowed');
-      
-    localStorage.setItem('wallet', accounts[0]);
-    return accounts[0];
+  const web3 = new Web3(window.ethereum);
+  const accounts = await web3.eth.requestAccounts();
+  if (!accounts || !accounts.length)
+    throw new Error("Wallet not found/allowed");
+
+  localStorage.setItem("wallet", accounts[0]);
+  return accounts[0];
 }
 
-function getContract(){
-    const wallet = localStorage.getItem('wallet');
-    if(!wallet) throw new Error('Unauthorized');
+function getContract() {
+  const wallet = localStorage.getItem("wallet");
+  if (!wallet) throw new Error("Unauthorized");
 
-    const web3 = new Web3(window.ethereum);
-    return new web3.eth.Contract( ABI, CONTRACT_ADDRAESS, { from: wallet });
+  const web3 = new Web3(window.ethereum);
+  return new web3.eth.Contract(ABI, CONTRACT_ADDRESS, { from: wallet });
 }
 
-export async function getCurrentVoting(){
-    const contract = getContract();
-    const voting = await contract.methods.getCurrentVoting().call();
-    return voting;
+export async function getCurrentVoting() {
+  const contract = getContract();
+  return contract.methods.getCurrentVoting().call();
 }
 
 export async function addVote(choice) {
-    const contract = getContract();
-    return contract.methods.addVote(choice).send();
+  const contract = getContract();
+  return contract.methods.addVote(choice).send();
 }
